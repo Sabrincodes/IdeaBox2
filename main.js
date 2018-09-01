84ecbb953886f30109328d7f989f36f554b9ec4b
David Cisneros <Dcisneros101@gmail.com>
2018-09-01 14:35:11 -0600

    create var

---
+++ main.js
@@ -0,0 +1,6 @@
+var ideaTitle = $('.title-input');
+var ideaBody = $('.body-input');
+var searchBar = $('.search');
+var ideaCardSection = $('.idea-card-section')
+
+ideaTitleInput.on('keyup', createIdea)
