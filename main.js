3e8d631c9ef97e544604211b4f12d20f824c904c
sashajustice <sabrinnasreddin@gmail.com>
2018-09-02 17:08:25 -0600

    Create enableSave function

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 
+var ideaTitle = $('.title-input');
+var ideaBody = $('.body-input');
+var searchBar = $('.search');
+var ideaCardSection = $('.idea-card-section');
+
+
+// Event listeners=============
+ideaTitleInput.on('keyup', createIdea);
+saveBtn.on('click', saveOurIdea);
+
+
 // functions==========================
 
 function Idea(id,title,body,quality) {
     this.id = id;
     this.title = $('.title-input').val();
     this.body = $('.body-input').val();
-    this.quality = “genius”;
+    this.quality = “swill”;
+}
+
+function saveOurIdea(e) {
+    e.preventDefault();
+    var anIdea = new Idea();
+
 }
 
 
 
-var ideaTitle = $('.title-input');
-var ideaBody = $('.body-input');
-var searchBar = $('.search');
-var ideaCardSection = $('.idea-card-section')
 
-ideaTitleInput.on('keyup', createIdea)
 
